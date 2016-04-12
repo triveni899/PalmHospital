6e21d4feffa5c0dbd7a2f60b9bbae7f8f8497ab7
triveni89 <tbanpel1@binghamton.edu>
2016-04-11 23:48:10 -0400

    AI player added

--- p07-banpela/blackjack_tutorial/ViewController.h
+++ p07-banpela/blackjack_tutorial/ViewController.h
@@ -8,19 +8,31 @@
 
 @interface ViewController : UIViewController{
     NSMutableArray *allImageViews;
-
+    int flag_player;
 }
 
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *HitButton;
+
 @property (weak, nonatomic) IBOutlet UILabel *dealerLabel;
 @property (weak, nonatomic) IBOutlet UILabel *playerLabel;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *standButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *resetButton;
+@property (weak, nonatomic) IBOutlet UILabel *AIplayerLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *HitButton;
+@property (weak, nonatomic) IBOutlet UIButton *standButton;
+@property (weak, nonatomic) IBOutlet UIButton *resetButton;
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSMutableArray *bustedPlayer;
+
+
+//@property (weak, nonatomic) IBOutlet UIButton *AIHitButton;
+//@property (weak, nonatomic) IBOutlet UIButton *AIStandButton;
 
 @property NSMutableArray *allImageViews;
+@property int flag_player;
 
 - (IBAction)HitCard:(id)sender;
 - (IBAction)Stand:(id)sender;
 - (IBAction)ResetGame:(id)sender;
+//- (IBAction)AIHitCard:(id)sender;
+//- (IBAction)AIStand:(id)sender;
+
 
 @end
